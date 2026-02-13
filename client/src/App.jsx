e369fa0be02c3c0fd3a12ee88c7a9557bb1a73aa
roshni-spec <roshanipari577@gmail.com>
2026-02-13 15:25:40 +0530

    basic porfolio functionalities and setup

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import SkillsPage from './pages/SkillsPage';
+import ProjectsPage from './pages/ProjectsPage';
+import ContactPage from './pages/ContactPage';
+
+function App() {
+  return (
+    <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/skills" element={<SkillsPage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
